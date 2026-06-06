1f3d56c770928eecb83f115c6554363c9775fbed
yadavd500 <yadavd500@gmail.com>
2026-06-06 12:01:46 +0000

    variable

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234567   
+let accountEmail = "123@google.com"
+accountCity = "delhi"
+accountPassword = "123456"
+
+
+//accountId = 2 // not allowed
+
+console.log(accountCity)
+
+//to print multiple value //
+
+console.table([accountEmail,accountId,accountCity,accountPassword])
