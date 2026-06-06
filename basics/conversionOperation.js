dd2af45e5101cea60c101cd826886f1f222003ef
yadavd500 <yadavd500@gmail.com>
2026-06-06 13:09:10 +0000

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,21 @@
+let score = 53
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let marks = "52"
+let maths = 56
+let science = 62
+console.table([marks, maths, science])
+
+let valueInNumber   = Number(marks)
+console.log(typeof valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn   =   Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 = true; 0 = false
+// ""= true
+// "yadav" = true
