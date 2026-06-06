52b68a9130aa6d6a997dfd5536f5551ab7e9a57e
yadavd500 <yadavd500@gmail.com>
2026-06-06 13:22:39 +0000

    operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,21 +1,36 @@
-let score = 53
+//let score = 53
 
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
 let marks = "52"
 let maths = 56
 let science = 62
-console.table([marks, maths, science])
+//console.table([marks, maths, science])
 
 let valueInNumber   = Number(marks)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn   =   Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1 = true; 0 = false
 // ""= true
 // "yadav" = true
+
+//****************operations******************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+console.log(2+3)
+console.log(52+2)
+console.log(20/2)
+console.log(2**2)
+console.log(2*2)
+console.log(20%3)
+console.log( 2==5)
+
